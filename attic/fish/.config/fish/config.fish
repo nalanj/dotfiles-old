83a0d794b04a07cbc2a29a751f3865adf6745ff2
Alan Johnson <nalanj@fastmail.com>
2019-03-23 06:33:16 -0400

    Added fish

--- attic/fish/.config/fish/config.fish
+++
@@ -1,4 +0,0 @@
-set -gx PATH $HOME/bin /opt/go/bin $PATH
-set -gx GOPATH $HOME
-set -gx GOROOT /opt/go
-set -gx EDITOR /usr/bin/vim
---
+++ home/.config/fish/config.fish
@@ -0,0 +1,5 @@
+set -gx PATH $HOME/bin /opt/go/bin /usr/local/go/bin $PATH
+set -gx GOROOT /opt/go
+set -gx EDITOR /usr/bin/vim
+set -gx GO111MODULE on
+
