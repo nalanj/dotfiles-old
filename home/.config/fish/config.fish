91d90aa75ee38ec4dc94d6ccd72c6571dff2b74c
Alan Johnson <nalanj@fastmail.com>
2019-05-06 11:13:17 -0400

    Set GOBIN

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,7 @@
-set -gx PATH $HOME/bin /opt/go/bin /usr/local/go/bin $HOME/go/bin $HOME/.local/bin $PATH
+set -gx PATH $HOME/bin /opt/go/bin /usr/local/go/bin $HOME/.local/bin $PATH
 set -gx EDITOR /usr/bin/vim
 set -gx GO111MODULE on
 set -gx GOROOT /usr/local/go
+set -gx GOBIN $HOME/.local/bin
 
 alias vim "gvim -v"
